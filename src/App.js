d59169ab0677265ecd0e5c90e8386cc5fde8351a
Prachi Rakhunde <rakhundeprachi03@gmail.com>
2026-08-05 11:37:42 +0530

    update the changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async"; 
 import SEO from "./components/SEO";
-
+import PublishedItineraries from './components/PublishedItineraries';
 import Header from './components/Header';
 import VideoSlider from './components/VideoSlider';
 import TrendingDestinations from './components/TrendingDestinations';
@@ -346,6 +346,7 @@ function App() {
               <TrendingDestinations />
               <IndiaDestinations/>
               <Adventures />
+              <PublishedItineraries />
               <Fixeddepartures />
               <Reviews />
               <QueryBox />
