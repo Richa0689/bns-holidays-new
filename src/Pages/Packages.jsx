1e11f7bfe842264bb1edc6f68b17a135a8b2f3ef
Richa0689 <richa.priyadarshi0689@gmail.com>
2026-08-30 04:21:13 +0530

    Change agencySlug in TravelPackages component

--- src/Pages/Packages.jsx
+++ src/Pages/Packages.jsx
@@ -4,7 +4,7 @@ export default function Packages() {
   return (
     <div style={{ maxWidth: 1200, margin: "0 auto", padding: "40px 20px" }}>
       <h1>Our Travel Packages</h1>
-      <TravelPackages agencySlug="abc-travels" />
+      <TravelPackages agencySlug="bns-holidays" />
     </div>
   );
 }
